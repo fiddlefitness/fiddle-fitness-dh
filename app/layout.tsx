f7f3c6f32b6c5be7c9bc260a5b47404c72761887
fiddlefitness <fitnessfiddle975@gmail.com>
2025-07-02 11:38:51 +0530

    Fixed mobile navbar and updated footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Script from 'next/script'
 
+console.log("ccsss")
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
